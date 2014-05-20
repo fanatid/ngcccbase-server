357a654d3db93880f9f23c9a4e886df7cee6cb33
Kirill Fomichev <fanatid@ya.ru>
2014-05-20 12:04:31 +0600

    add tests and change urls to methods

--- lib/transport/http.py
+++ lib/transport/http.py
@@ -1,37 +1,63 @@
+import json
+
 from twisted.web.resource import Resource
 from twisted.web.server import Site, NOT_DONE_YET
 from twisted.internet.defer import Deferred
 
 
-class BaseResource(Resource):
+class RootResource(Resource):
+    isLeaf = True
+
     def __init__(self, blockchain):
         self.blockchain = blockchain
 
+    def render_POST(self, request):
+        try:
+            query = json.loads(request.content.read())
+        except (ValueError, TypeError):
+            request.setResponseCode(400)
+            return 'JSON loads error'
 
-class LeafResource(BaseResource):
-    isLeaf = True
+        if 'method' not in query:
+            request.setResponseCode(400)
+            return 'method not in request'
+        if 'params' not in query:
+            request.setResponseCode(400)
+            return 'params not in request'
+        if not isinstance(query['params'], list):
+            request.setResponseCode(400)
+            return 'params not list'
 
+        method, params = query['method'], query['params']
+        if method == 'getblockcount':
+            return self.getblockcount(request, params)
 
-class BlockCount(LeafResource):
-    def _callback(self, result, request):
-        request.write(str(result))
-        request.finish()
+        request.setResponseCode(400)
+        return 'method not found'
 
-    def _errback(self, fail=None, request=None):
+    def _common_errback(self, fail=None, request=None):
         if request is None:
             fail, request = None, fail
-        request.write(str(fail))
+        request.write(json.dumps({
+            'result': None,
+            'error': str(fail),
+        }))
         request.finish()
 
-    def render_GET(self, request):
+    def getblockcount(self, request, params):
         d = Deferred()
-        d.addCallback(self._callback, request)
-        d.addErrback(self._errback, request)
+        d.addCallback(self._getblockcount_callback, request)
+        d.addErrback(self._common_errback, request)
         self.blockchain.get_block_count(d)
         return NOT_DONE_YET
 
+    def _getblockcount_callback(self, result, request):
+        request.write(json.dumps({
+            'result': result,
+            'error': None,
+        }))
+        request.finish()
+
 
 def get_HTTPFactory(config, blockchain):
-    root = Resource()
-    root.putChild('blockcount', BlockCount(blockchain))
-    return Site(root)
+    return Site(RootResource(blockchain))
---
+++ tests/test_http.py
@@ -0,0 +1,29 @@
+import unittest
+import urllib2
+import json
+
+
+class TestsTransportHTTP(unittest.TestCase):
+    def setUp(self):
+        self.server_url = "http://localhost:28832/"
+
+    def call(self, method, params=None):
+        data = json.dumps({
+            'method': method,
+            'params': [] if params is None else params,
+        })
+        req = urllib2.Request(
+            self.server_url,
+            data,
+            {'Content-Type': 'application/json'}
+        )
+        return json.loads(urllib2.urlopen(req).read())
+
+    def test_blockcount(self):
+        response = self.call('getblockcount')
+        self.assertTrue(isinstance(response['result'], int))
+        self.assertTrue(response['result'] > 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
